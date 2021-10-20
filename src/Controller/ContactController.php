cb039f028f0efb67c000c19c38b3d527e01cfb2c
Clay <c.clelia@live.fr>
2021-10-20 13:12:02 +0200

    first

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+            
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('ton@gmail.com')
+                ->subject('vous avez reçu un email du site Promenade Sucrée')
+                ->text('Sender : '.$contactFormData['email'].\PHP_EOL.
+                    $contactFormData['Message'],
+                    'text/plain');
+            $mailer->send($message);
+            $this->addFlash('success', 'Vore message a été envoyé');
+            return $this->redirectToRoute('contact');
+        }
+        return $this->render('contact/index.html.twig', [
+            'contactForm' => $form->createView()
+        ]);
+    }
+}
+
